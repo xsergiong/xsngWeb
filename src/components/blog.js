9a545530058312319bcc7984fd22198f23b326ec
xs3rg1ong <snunogomez@gmail.com>
2026-07-27 21:36:13 +0200

    Publication

---
+++ src/components/blog.js
@@ -0,0 +1,32 @@
+const root = document.querySelector("#blog");
+
+function Load()
+{
+    fetch('../../assets/json/blog-entries.json')
+        .then(response => response.json())
+        .then(data => {
+            for (let i = 0; i < data.length; i++)
+            {
+                let entry = document.createElement('div');
+                let title = document.createElement('h3');
+                let content = document.createElement('p');
+                let time = document.createElement('p');
+
+                title.textContent = data[i].title;
+                content.textContent = data[i].content;
+                time.textContent = data[i].time + " " + data[i].date;
+                
+                entry.setAttribute('class', 'blog-entry');
+                time.setAttribute('class', 'fade-text');
+
+                entry.appendChild(title);  
+                entry.appendChild(content);
+                entry.appendChild(time);
+
+                root.appendChild(entry);
+            }
+        }
+    );
+}
+
+Load();
